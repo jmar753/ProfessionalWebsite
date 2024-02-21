122a179f50ddec72a33c23c421d4106cb9e5b612
Jr Mohan <jmar7533@gmail.com>
2024-02-20 22:42:22 -0500

    remove layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import "@/styles/globals.css";
-import Header from "@/components/Header";
-import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
