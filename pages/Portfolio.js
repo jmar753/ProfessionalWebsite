d00b1c7b04110e367a0130ecc6ac83bace02de57
Jr Mohan <jmar7533@gmail.com>
2024-02-28 18:09:37 -0500

    set Projects

--- pages/Portfolio.js
+++ pages/Portfolio.js
@@ -1,10 +1,9 @@
+import Link from "next/link"
+
 export default function Portfolio() {
 
     const websiteArray = [
-        {name:"example"},
-        {name:"example"},
-        {name:"example"},
-        {name:"example"},
+        {name:"Museum Search", link:"https://jr-mohan-assignment-6.vercel.app/", imageLoc:"/portfolio/museum.jpg"},
     ]
 
     return (
@@ -16,25 +15,27 @@ export default function Portfolio() {
                 <div className="flex flex-wrap justify-center">
 
                     {websiteArray.map((item, index) => (
-                    <div className="p-4 max-w-2xl" key={index}>
-                        <div className="hover:cursor-pointer flex rounded-lg shadow-xl h-full flex-col transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300">
-                            
-                            <img className="object-cover col-span-2 w-36 h-36 rounded-lg m-4" src={`/static/forest.jpg`} alt=""/>
-                            
-                            {/* Skills*/}
-                            <p className="text-neutral-500 leading-relaxed text-lg font-bold px-4">
-                                Next.js & Bootstrap
-                            </p>
+                    <Link href="https://jr-mohan-assignment-6.vercel.app/">
+                        <div className="p-4 max-w-2xl" key={index}>
+                            <div className="hover:cursor-pointer flex rounded-lg shadow-xl h-full flex-col transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300">
+                                
+                                <img className="object-cover col-span-2 rounded-lg m-4" src={item.imageLoc} alt=""/>
+                                
+                                {/* Skills*/}
+                                <p className="text-neutral-500 leading-relaxed text-lg font-bold px-4">
+                                    Next.js & Bootstrap
+                                </p>
 
-                            {/* Title*/}
-                            <p className="leading-relaxed text-2xl font-bold px-4">
-                                Museum Page with Working Back-End
-                            </p>
-                            <div className="py-6">
+                                {/* Title*/}
+                                <p className="leading-relaxed text-2xl font-bold px-4">
+                                    Museum Page with Working Back-End
+                                </p>
+                                <div className="py-6">
 
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </Link>
                     ))}
 
                 </div>
